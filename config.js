6e1047b1895ecf6e2065d3959eeecacc97dc1095
sippah750 <sadarusankalpa2007@gmail.com>
2025-03-02 09:37:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "KQQiTB5D#Yc53FTmN8UL4Gr8FC2Wi7dnbtOkYhRT8MB3ObJFxLww", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", //true or false 
